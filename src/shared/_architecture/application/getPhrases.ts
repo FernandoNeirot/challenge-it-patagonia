d229e4f008f521712f3ad8b89f42c8b01beb36d5
Fernando-Neirot <fneirot@confidentally.com>
2025-02-06 15:17:33 -0300

    sort data

--- src/shared/_architecture/application/getPhrases.ts
+++ src/shared/_architecture/application/getPhrases.ts
@@ -3,6 +3,8 @@ import { apiGetPhrases } from "../infrastructure/phrases.api";
 
 export const getPhrases = async (id:string): Promise<ResponseGetPhrases> => {
   const response = await apiGetPhrases(id);
+  const dataSorted = response.data?.sort((a, b) => b.created.localeCompare(a.created));
+  response.data = dataSorted??[];
   //-----------------------------------------------------------------------------------------------
   // Se agrega alguna logica de negocio sobre la respuesta para enviarle a la capa de presentación
   //-----------------------------------------------------------------------------------------------
